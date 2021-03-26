edc39db8b7d6b6723e79a027c234f50edecd7d42
MiniHack <MiniHack@onlineminihack.github.io>
2021-03-26 20:42:01 +0000

    Publish

--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a23e8ee5483883c97a01.js"
+    "url": "webpack-runtime-712746248faf10b34265.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "aa15cb96387e008ec39e8365d06b52b7"
+    "revision": "955004d4dab82d1c419664e2ca0126d8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
